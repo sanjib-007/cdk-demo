e6e7e6a7422841a82c87c8bd21cb5d0b1f4a08a5
sanjib-007 <113636141+sanjib-007@users.noreply.github.com>
2022-09-16 14:32:22 -0400

    Update cdk-demo-stack.ts

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -2,6 +2,7 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export class CdkDemoStack extends Stack {
@@ -15,5 +16,12 @@ export class CdkDemoStack extends Stack {
     const topic = new sns.Topic(this, 'CdkDemoTopic');
 
     topic.addSubscription(new subs.SqsSubscription(queue));
+    
+    const defaultvpc =ec2.Vpc.fromLookup(this, 'vpc',{isDefault: true})
+    const ami= new ec2.LookupMachineImage({name:'ZytbImg010422',
+               owners:[' 925975727637']
+    })
+   const ec2instance = new ec2.Instance(this, 'sample-ec2', {instanceType:new ec2.InstanceType('t2.micro'),
+                       machineImage:ami,vpc:defaultvpc})
   }
 }
